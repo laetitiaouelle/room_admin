85aec061b7676fec7072c4ec68baa128d0126971
laetitiaouelle <ouelle123ab@gmail.com>
2022-09-15 18:13:54 +0000

    latest update8

--- states/panorama_context.js
+++ states/panorama_context.js
@@ -1,3 +1,3 @@
 import {createContext} from "react";
 
-export const PanoramaContext = createContext(null)
+export const PanoramaContext = createContext()
--- states/viewport_context.js
+++ states/viewport_context.js
@@ -1,2 +1,2 @@
 import {createContext} from "react";
-export const ViewPortContext = createContext(null)
+export const ViewPortContext = createContext()
